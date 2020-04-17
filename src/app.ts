abf384022a21fc760149e40faf5d9c97b43e6f16
Andres Maradiaga <andres.maradiaga97@gmail.com>
2020-04-17 17:32:53 -0600

    .env added/ installed dotenv and set the path to connect to MongoDB

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import express, {Application} from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import { MainController } from "./controllers/main.controller";
+import mongoose from "mongoose";// Con esto se hace la configuracion para conectarnos a la base de datos
+
+
+import {config} from "dotenv"; //Cada linea de este archivo las va a tratar y manipular como si fuesen variables
+import {resolve} from "path"; // Resuelve para que un archivo pueda ser escuchado desde otro archivo y poder acceder a el
+config({path:resolve(__dirname, "../.env")}); //Con esto resolvemos nuestro archivo .env para que pueda ser escuchado desde cualquier lado de mi directorio
 
 class App{
     public app: Application; //Aplication es una interfaz
@@ -9,13 +15,26 @@ class App{
     constructor(){
         this.app = express(); //con esto se crea e inicializa la parte de la app
         this.setConfig();
+        this.setMongoDBConfig();
         this.mainController = new MainController (this.app);
     }
-    
+
     private setConfig(){ //aqui se hacen las manipulaciones de nuestra app
         this.app.use(bodyParser.json({limit: "50mb"})); 
         this.app.use(bodyParser.urlencoded({limit:"50mb", extended:true})); //si trabajas con formularios, el extended se utiliza para poder trabajar con www-form-urlencoded
         this.app.use(cors());
+    }
+
+    private setMongoDBConfig(){
+        mongoose.Promise = global.Promise; //A nivel global, mientras no se cumpla todo lo que esta abajo, no vamos a continuar con las siguientes lineas (propio de MongoDB)
+       
+        mongoose.connect(process.env.MONGO_URI!,{ useNewUrlParser:true, useUnifiedTopology: true }, (err: any)=>{
+            if(err){
+                console.log(err.message);
+            }else{
+                console.log("Conexion exitosa");
+            }
+        });
     }
 }
 
