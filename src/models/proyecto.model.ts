0d11ef4049cc1d6886128c39c9ff5f5e0e12b4ee
Moises Nuñez <moyonuez1@gmail.com>
2020-05-11 00:50:42 -0600

    Corrigiendo datos del proyecto

--- src/models/proyecto.model.ts
+++ src/models/proyecto.model.ts
@@ -1,20 +1,25 @@
 import mongoose from "mongoose";
 
 export interface IProyecto extends mongoose.Document{
-    nombre: string;
-    password: string;
-    telefono: string;
-    direccion: string;
+    id_proyecto:string;
+    nombre_proyecto: string;
+    fecha: string;
+    sede: string;
+    etapa: string;
     zona_intervencion: string;
+    num_asistencia: number;
+    //Interventor:string;
 }
 
 const ProyectoSchema = new mongoose.Schema({
     //_id: {type: String, required:true},
-    nombre: {type: String, required:true},
-    password: {type: String, required:true},
-    telefono: {type: String, required:true},
-    direccion: {type: String, required:true},
-    zona_intervencion: {type: String, required:true}
+    id_proyecto: {type: String, required: true},
+    nombre_proyecto: {type: String, required:true},
+    fecha: {type: String, required:true},
+    sede: {type: String, required:true},
+    etapa: {type: String, required:true},
+    zona_intervencion: {type: String, required:true},
+    num_asistencia: {type: Number, required:true}
 });
 
 export const Proyecto = mongoose.model<IProyecto>("Proyecto", ProyectoSchema);
